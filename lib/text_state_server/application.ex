dac68a436c181bdd392148eb676b8fd62edeb2f6
Hentioe <me@bluerain.io>
2021-07-22 18:23:59 +0800

    Add otp app module

---
+++ lib/text_state_server/application.ex
@@ -0,0 +1,15 @@
+defmodule TextStateServer.Application do
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    children = []
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: TextStateServer.Supervisor]
+
+    Supervisor.start_link(children, opts)
+  end
+end
--- mix.exs
+++ mix.exs
@@ -14,6 +14,7 @@ defmodule TextStateServer.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
+      mod: {TextStateServer.Application, []},
       extra_applications: [:logger]
     ]
   end
