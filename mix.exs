2f99aa115e3a93bca33e0f197b59ace51bfaea97
Hentioe <me@bluerain.io>
2021-07-25 13:29:57 +0800

    Update version

--- mix.exs
+++ mix.exs
@@ -1,10 +1,12 @@
 defmodule TextStateServer.MixProject do
   use Mix.Project
 
+  @version "1.0.0-dev"
+
   def project do
     [
       app: :text_state_server,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps()
